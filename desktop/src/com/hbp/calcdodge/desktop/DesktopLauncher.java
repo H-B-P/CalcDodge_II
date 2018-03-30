a77328a658b2a3feb87adc853171234bf22c823f
H-B-P <abstraction.application@protonmail.com>
2018-03-30 17:06:17 +0100

    there has to be a better way to handle this kind of transition and some day I may even find it

---
+++ desktop/src/com/hbp/calcdodge/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.hbp.calcdodge.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hbp.calcdodge.CalcDodge;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CalcDodge(), config);
+	}
+}
